192ce4d445403c8e55c9b04958c146da7857c34d
erenkaya <erenkaya1993@gmail.com>
2018-11-02 11:16:07 +0300

    method reference test in javascript

--- srcjsx/components/Main.js
+++ srcjsx/components/Main.js
@@ -1,76 +1,89 @@
-import React from 'react'
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
- class Main extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            user:"Eren",
-            pwd:""
-        }
-        this.onClickSubmitLoginForm = this.onClickSubmitLoginForm.bind(this);
-        this.onChange = this.onChange.bind(this);
-    }
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: "Eren",
+      pwd: ""
+    };
+    this.onClickSubmitLoginForm = this.onClickSubmitLoginForm.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.testMethod = this.testMethod.bind(this);
+  }
 
-    componentWillMount(){
-     
-    }
+  testMethod(object) {
+    object.push("str2");
+    window.objectTestMethod = object;
+  }
+  componentWillMount() {
+    let object = [];
+    object.push("str1");
+    this.testMethod(object);
+    window.objectWillMount = object;
+  }
 
-    onClickSubmitLoginForm(){
-        let data = new FormData();
-        data.append("users",this.state.user);
-        data.append("pwd",this.state.pwd);
-        console.log(data);
-        axios({
-            method:'post',
-            url:'Basic-Servlet/LoginServlet',
-            data:this.toJson(data),
-            credentials: 'same-origin',
-            headers: {
-                'content-type': 'application/x-www-form-urlencoded;charset=utf-8',
-                "Access-Control-Allow-Credentials": true,
-                "Access-Control-Allow-Origin": "*",
-                "Cache-Control": "no-cache"
-              }
-        }).then((success)=>{
-            console.log(success);
-            // this.props.history.push('/LogoutServlet');
-            window.location = '/Basic-Servlet/LogoutServlet';
-            
-        }).catch((err)=>{
-            console.log(err);
-            
-        })
-    }
-    toJson(formData) {
-        var object = {};
-        formData.forEach(function (value, key) {
-            object[key] = value;
-        });
-        return JSON.stringify(object);
-    }
+  onClickSubmitLoginForm() {
+    let data = new FormData();
+    data.append("users", this.state.user);
+    data.append("pwd", this.state.pwd);
+    console.log(data);
+    axios({
+      method: "post",
+      url: "/LoginServlet",
+      data: this.toJson(data),
+      credentials: "same-origin",
+      headers: {
+        "content-type": "application/x-www-form-urlencoded;charset=utf-8",
+        "Access-Control-Allow-Credentials": true,
+        "Access-Control-Allow-Origin": "*",
+        "Cache-Control": "no-cache"
+      }
+    })
+      .then(success => {
+        console.log(success);
+        // this.props.history.push('/LogoutServlet');
+        window.location = "/LogoutServlet";
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+  toJson(formData) {
+    var object = {};
+    formData.forEach(function(value, key) {
+      object[key] = value;
+    });
+    return JSON.stringify(object);
+  }
 
-    componentDidUpdate(){
-        console.log(this.state);
-    }
+  componentDidUpdate() {
+    console.log(this.state);
+  }
 
-    onChange(e){
-        const name = e.target.name;
-        const value = e.target.value;
-        this.setState((prevState) => ({
-            ...prevState,
-            [name]:value
-        }));
-    }
+  onChange(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  }
 
-    render(){
-        return (
-            <div>
-                <input onChange={this.onChange} name="user" type="text" value={this.state.user}></input>
-                <input onChange={this.onChange} name="pwd" type="password" ></input>
-                <input type="submit" onClick={this.onClickSubmitLoginForm}></input>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <input
+          onChange={this.onChange}
+          name="user"
+          type="text"
+          value={this.state.user}
+        />
+        <input onChange={this.onChange} name="pwd" type="password" />
+        <input type="submit" onClick={this.onClickSubmitLoginForm} />
+      </div>
+    );
+  }
 }
 export default Main;
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,40 @@
-const path = require('path');
-var buildPath = 'WebContent/public/scripts';
-var devServerPath = "WebContent"
-module.exports =  {
-  entry: './srcjsx/app.js',
+const path = require("path");
+var buildPath = "WebContent/public/scripts";
+var devServerPath = "WebContent";
+module.exports = {
+  entry: "./srcjsx/app.js",
   output: {
     path: path.join(__dirname, buildPath),
-    filename: 'bundle.js'
+    filename: "bundle.js"
   },
   module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/,
-      query: {
-        presets: ['react', 'es2015']
+    rules: [
+      {
+        loader: "babel-loader",
+        test: /\.js$/,
+        exclude: /node_modules/,
+        query: {
+          presets: ["react", "es2015"]
+        }
+      },
+      {
+        test: /\.s?css$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
       }
-    }, {
-      test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
+    ]
   },
-  devtool: 'cheap-module-eval-source-map',
+  devtool: "cheap-module-eval-source-map",
   devServer: {
     contentBase: buildPath,
-    proxy: [{
-      // index: '/Basic-Servlet',
-      context: [
-        '/LoginServlet',
-        '/LogoutServlet',
-        '/Basic-Servlet'
-        
-      ],
-      target: `http://127.0.0.1:8081/`,
-      // secure: false,
-      // changeOrigin: true,
-      headers: { host: 'localhost:4200' }
-    }]
-  },
+    proxy: [
+      {
+        // index: '/Basic-Servlet',
+        context: ["/LoginServlet", "/LogoutServlet", "/basic-servlet"],
+        target: `http://127.0.0.1:8080/`,
+        // secure: false,
+        // changeOrigin: true,
+        headers: { host: "localhost:4200" }
+      }
+    ]
+  }
 };
